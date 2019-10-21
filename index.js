e173c54f26ce2b403ae3f001131957aacfbb2881
Sonofarobin <zanthrobinson@gmail.com>
2019-10-21 21:42:06 +0000

    added index file

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./mid');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle
+};
